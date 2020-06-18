a50b2e5584c4d92872f238e0c935333203f6a7f8
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-06-18 17:08:05 +0600

    Spread operator

--- 02-Next gen TS/dist/app.js
+++ 02-Next gen TS/dist/app.js
@@ -2,12 +2,23 @@
 const userName = 'Bob';
 let age = 30;
 age = 29;
-const add = (a, b) => a + b;
+const add = (a, b = 3) => a + b;
 console.log(add(4, 7));
+console.log(add(6));
 const printOutput = output => console.log(output);
 printOutput(add(4, 9));
 const button = document.querySelector('button');
 if (button) {
     button.addEventListener('click', event => console.log(event));
 }
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+const Person = {
+    name: 'Bob',
+    age: '30'
+};
+const copiedPerson = Object.assign({}, Person);
+console.log(copiedPerson);
 //# sourceMappingURL=app.js.map
--- 02-Next gen TS/src/app.ts
+++ 02-Next gen TS/src/app.ts
@@ -6,10 +6,11 @@ let age = 30;
 age = 29;
 
 
-const add = (a: number, b: number) => a + b;
+const add = (a: number, b: number = 3) => a + b;
 
 
 console.log(add(4, 7));
+console.log(add(6));
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 printOutput(add(4, 9));
@@ -20,3 +21,17 @@ if (button) {
     button.addEventListener('click', event => console.log(event));
 }
 
+const hobbies = ['Sports', 'Cooking'];
+
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const Person = {
+    name: 'Bob',
+    age: '30'
+};
+
+const copiedPerson = { ...Person };
+console.log(copiedPerson);
